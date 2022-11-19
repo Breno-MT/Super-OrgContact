561edb9ec5dfeae6c97b87169f7d8dbd4e4e8644
Breno-MT <hentgenp@gmail.com>
2022-11-19 18:31:32 -0300

    feat: criado models para collection

---
+++ BackEnd-Python/src/models/users.py
@@ -0,0 +1,37 @@
+users_validator = {
+    "$jsonSchema": {
+        "bsonType": "object",
+        "required": ["_id", "email", "password", "domain"],
+        "properties": {
+            "_id": {
+                "bsonType": "objectId",
+                "description": "Chave definida da collection",
+            },
+            "email": {
+                "bsonType": "string",
+                "pattern": "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+[.]+[a-zA-Z]{2,10}$",
+                "description": "Email do usuário",
+            },
+            "password": {
+                "bsonType": "string",
+                "pattern": "^[a-zA-Z0-9.!@#$%&*/_%+-]{8,99}$",
+                "description": "Senha do usuário",
+            },
+            "domain": {
+                "bsonType": "objectId",
+                "description": "Vinculo da domain gmail",
+            },
+        },
+    }
+}
+
+def create_collection_users(mongo_client):
+    try:
+        print("Criando Users collection.")
+        mongo_client.create_collection("users")
+        print("Users criada com sucesso.")
+
+    except Exception as e:
+        print(e)
+
+    mongo_client.command("collMod", "users", validator=users_validator)
