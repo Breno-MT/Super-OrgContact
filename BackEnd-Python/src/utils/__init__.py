cf0011ca953256e28a25bcb57a792a3c0ed4d17f
Breno-MT <hentgenp@gmail.com>
2022-11-19 16:37:44 -0300

    feat: criado gerador de jwt

--- BackEnd-Python/src/utils/__init__.py
+++ BackEnd-Python/src/utils/__init__.py
@@ -1,3 +1,9 @@
 from flask_pymongo import PyMongo
+from flask import current_app
+from jwt import encode
 
 mongo = PyMongo()
+
+def generate_jwt(payload):
+    token = encode(payload, current_app.config["SECRET_KEY"], "HS256")
+    return token
