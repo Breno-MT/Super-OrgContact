f0560bdc0d269e72fb77a2559ce9afbad78f143e
Breno-MT <hentgenp@gmail.com>
2022-11-19 18:31:15 -0300

    feat: criado command para adicionar collections

--- BackEnd-Python/app.py
+++ BackEnd-Python/app.py
@@ -1,7 +1,19 @@
-from src import app
+import click
+from flask.cli import with_appcontext
+from src import app, mongo_client
 from src.routes import routes
+from src.models.users import create_collection_users
+from src.models.domain import create_collection_domain
 
 routes(app=app)
+
+@click.command(name="create_collections")
+@with_appcontext
+def command_collection():
+    create_collection_users(mongo_client)
+    create_collection_domain(mongo_client)
+
+app.cli.add_command(command_collection)
 
 if __name__ == "__main__":
     app.run()
