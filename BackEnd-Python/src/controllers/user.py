bac12a4da7d975ada854ce5645075538c7b34ca9
Breno-MT <hentgenp@gmail.com>
2022-11-21 11:40:41 -0300

    feat: criado as endpoints da people api

--- BackEnd-Python/src/controllers/user.py
+++ BackEnd-Python/src/controllers/user.py
@@ -5,13 +5,29 @@ from flask.globals import session
 from google import auth
 from google.oauth2 import id_token
 from src.services.oauth_services import callback_google, flow
+from src.services.people_api_services import people_api_paginate_10
+from src.services.people_api_services import people_api_all_person_connection
 
 users = Blueprint("users", __name__, url_prefix="/users")
 
-@users.route("/", methods=["GET"])
-def get_users():
+@users.route("/people_api/v1/get_all_connections", methods=["GET"])
+def get_all_person_connections():
+    """
+    From the Authenticated User, get all connections.
+    """
     return Response(
-        response=json_util.dumps({"records": "ok"}),
+        response=json_util.dumps(people_api_all_person_connection()),
+        status=200,
+        mimetype="application/json"
+    )
+
+@users.route("/people_api/v1/list_10_connections", methods=["GET"])
+def list_10_person_connection():
+    """
+    From the Authenticated User, list up to 10 person connections.
+    """
+    return Response(
+        response=json_util.dumps(people_api_paginate_10()),
         status=200,
         mimetype="application/json"
     )
