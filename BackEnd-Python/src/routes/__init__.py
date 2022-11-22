fad2f098a64c9d96def4e90d10c3cd858e268368
Breno-MT <hentgenp@gmail.com>
2022-11-22 10:32:21 -0300

    refact: adicionado rota de contacts

--- BackEnd-Python/src/routes/__init__.py
+++ BackEnd-Python/src/routes/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from src.controllers.user import users
+from src.controllers.contacts import contacts
 
 def routes(app: Flask):
     app.register_blueprint(users)
+    app.register_blueprint(contacts)
